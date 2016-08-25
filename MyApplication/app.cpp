280c184e7aa013d30b4b3699b876802eff17a48e
Student <Student@SM130-D0022968>
2016-08-24 20:29:38 -0700

    Add pow() to first col

--- MyApplication/app.cpp
+++ MyApplication/app.cpp
@@ -4,14 +4,13 @@
 
 #include <iostream>
 #include <string>
-#include <math>
 using namespace std;
 
 int main()
 {
-
+	
 	cout << "a   a^2   a^3" << endl;
-	cout << "1   1     1" << endl;
+	cout << "1 " << pow(1.0, 2) << " " << pow(1.0, 3) << endl;
 	cout << "2   4     8" << endl;
 	cout << "3   9     27" << endl;
 	cout << "4   16    64" << endl;
