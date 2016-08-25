40db76922319642aea075699f57f20e71f7799e3
Student <Student@SM130-D0022968>
2016-08-24 20:23:26 -0700

    Placeholder table

--- MyApplication/app.cpp
+++ MyApplication/app.cpp
@@ -1,1 +1,20 @@
-// a file to edit here
+//Programmer: Alex Berry
+//Date: August 24 2016
+//Purpose: Second Lab - Pow Usage
+
+#include <iostream>
+#include <string>
+#include <math>
+using namespace std;
+
+int main()
+{
+
+	cout << "a   a^2   a^3" << endl;
+	cout << "1   1     1" << endl;
+	cout << "2   4     8" << endl;
+	cout << "3   9     27" << endl;
+	cout << "4   16    64" << endl;
+
+	return 0;
+}
